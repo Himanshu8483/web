492cfac2f8d456595c55c8c1f70cad62a6742a15
Himanshu Kushwaha <himanshukushwahabkp@gmail.com>
2025-04-04 15:02:20 +0530

    all

--- Pro/script.js
+++ Pro/script.js
@@ -1,3 +1,71 @@
 
-let show= document.querySelector('.div1')
-show.innerHTML('world')
+const red=()=>{
+    let red = document.querySelector('#sec4')
+    red.style.backgroundColor='red';
+    let hide = document.querySelector('.red')
+    hide.style.opacity='0';
+    let blue = document.querySelector('.blue')
+    blue.style.opacity='1';
+    let green = document.querySelector('.green')
+    green.style.opacity='1';
+    let yellow = document.querySelector('.yellow')
+    yellow.style.opacity='1';
+    let pink = document.querySelector('.pink')
+    pink.style.opacity='1';
+}
+const green=()=>{
+    let green = document.querySelector('#sec4')
+    green.style.backgroundColor='green';
+    hide = document.querySelector('.green')
+    hide.style.opacity='0';
+    let blue = document.querySelector('.blue')
+    blue.style.opacity='1';
+    let yellow = document.querySelector('.yellow')
+    yellow.style.opacity='1';
+    let pink = document.querySelector('.pink')
+    pink.style.opacity='1';
+    let red = document.querySelector('.red')
+    red.style.opacity='1';
+}
+const yellow=()=>{
+    let yellow = document.querySelector('#sec4')
+    yellow.style.backgroundColor='yellow';
+    hide = document.querySelector('.yellow')
+    hide.style.opacity='0';
+    let blue = document.querySelector('.blue')
+    blue.style.opacity='1';
+    let green = document.querySelector('.green')
+    green.style.opacity='1';
+    let pink = document.querySelector('.pink')
+    pink.style.opacity='1';
+    let red = document.querySelector('.red')
+    red.style.opacity='1';
+}
+const blue=()=>{
+    let blue = document.querySelector('#sec4')
+    blue.style.backgroundColor='blue';
+    hide = document.querySelector('.blue')
+    hide.style.opacity='0';
+    let green = document.querySelector('.green')
+    green.style.opacity='1';
+    let yellow = document.querySelector('.yellow')
+    yellow.style.opacity='1';
+    let pink = document.querySelector('.pink')
+    pink.style.opacity='1';
+    let red = document.querySelector('.red')
+    red.style.opacity='1';
+}
+const pink=()=>{
+    let pink = document.querySelector('#sec4')
+    pink.style.backgroundColor='pink';
+    hide = document.querySelector('.pink')
+    hide.style.opacity='0';
+    let blue = document.querySelector('.blue')
+    blue.style.opacity='1';
+    let green = document.querySelector('.green')
+    green.style.opacity='1';
+    let yellow = document.querySelector('.yellow')
+    yellow.style.opacity='1';
+    let red = document.querySelector('.red')
+    red.style.opacity='1';
+}
